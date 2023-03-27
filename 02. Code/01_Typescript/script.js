d6b69d725106048441e55dfa2a0b13cd0cc48ef6
Miguel Campos <camposmiguel@gmail.com>
2023-03-27 11:47:30 +0200

    Ejercicios typescript

--- 02. Code/01_Typescript/script.js
+++ 02. Code/01_Typescript/script.js
@@ -1,3 +1,17 @@
-var x;
-x = 10;
-x = "Hello";
+var x = 10;
+var y;
+var PI = 3.14; // number
+x = 20;
+y = 500;
+y = "Hello";
+var z = 100;
+if (true) {
+    var condicion = false;
+}
+console.log(x);
+var mensaje = "Hola Mundo";
+var welcome = "bienvenido";
+var nombre = "Juan";
+// Template literals
+console.log("".concat(mensaje, ", ").concat(welcome, ": ").concat(nombre));
+var i;
--- 02. Code/01_Typescript/script.ts
+++ 02. Code/01_Typescript/script.ts
@@ -1,4 +1,26 @@
-let x: number;
+let x = 10;
+let y;
+const PI = 3.14; // number
 
-x = 10;
-x = "Hello";
+x = 20;
+y = 500;
+
+y = "Hello";
+
+var z = 100;
+
+if(true) {
+    let condicion = false;
+}
+
+
+console.log(x);
+
+let mensaje = "Hola Mundo";
+let welcome = "bienvenido";
+let nombre = "Juan";
+
+// Template literals
+console.log(`${mensaje}, ${welcome}: ${nombre}`);
+
+let i: number | undefined;
